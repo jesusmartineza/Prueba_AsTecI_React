1eda1817c7c6111424e8a341712022efc45dafdc
Jesus Martinez Ayala <jesusayala368@gmial.com>
2023-01-04 18:03:30 -0600

    Prueba Asteci React

--- src/helpers/ApiId.jsx
+++ src/helpers/ApiId.jsx
@@ -1,7 +1,10 @@
 //Consulta para obtener todos los datos de un registro por id
 
-export const ApiId = async () => {
-	const url = `https://api.datos.gob.mx/v1/condiciones-atmosfericas/`;
+// const id = '5952983359954a0adbf7ab09';
+const cityid = 'MXAS0002';
+
+export const ApiId = async (cityid) => {
+	const url = `https://api.datos.gob.mx/v1/condiciones-atmosfericas/${cityid}`;
 	const respuesta = await fetch(url);
 	const resultado = await respuesta.json();
 
